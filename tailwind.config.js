300547c55c7544fcae49c00deb5dd9fdf098df1d
Gil <gilvicentevieira@gmail.com>
2024-09-03 22:20:16 +0100

    configuring tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,40 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+		container: {
+			center: true,
+			padding: "15px",
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1200px",
+		},
+		fontFamily: {
+			primary: "var(--font-jetbrainsMono)"
+		},
   	extend: {
+			colors: {
+				primary: "#1c1c22",
+				accent: {
+					DEFAULT: "#00ff99",
+					hover: "#00e187"
+				}
+			},
+			keyframes: {
+				"accordion-down": {
+					from: {height: "0"},
+					to: {height: "var(--radix-accordion-content-height)"}
+				},
+				"accordion-up": {
+					from: {height: "var(--radix-accordion-content-height)"},
+					to: {height: "0"}
+				}
+			},
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
   			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
@@ -16,8 +47,7 @@ module.exports = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
+  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
