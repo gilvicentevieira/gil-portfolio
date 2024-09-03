3dd3bb1c35d46720f64c750649381fa0329da56d
Gil <gilvicentevieira@gmail.com>
2024-09-03 22:21:23 +0100

    adding Header to page

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link"
+
+// components
+import Nav from "./Nav"
+import { Button } from "./ui/button"
+import MobileNav from "./MobileNav"
+
+
+const Header = () => {
+  return (
+    <div className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        { /* logo */}
+        <Link href={"/"}>
+          <h1 className="text-4xl font-semibold">
+            Gil<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* desktop nav & hire me button*/}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        { /* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
