6a2a31138bff631f2e793e10c556905b92014a8a
Gil <gilvicentevieira@gmail.com>
2024-09-05 22:07:27 +0100

    updated stats with my values

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,10 +3,10 @@
 import CountUp from "react-countup"
 
 const stats = [
-  { num: 12, text: "Years of experience" },
-  { num: 26, text: "Projects completed" },
-  { num: 8, text: "Technologies mastered" },
-  { num: 500, text: "Code commits" }
+  { num: 7, text: "Years of experience" },
+  { num: 20, text: "Projects completed" },
+  { num: 5, text: "Technologies mastered" },
+  { num: 25, text: "Technologies" },
 ]
 
 const Stats = () => {
@@ -18,6 +18,7 @@ const Stats = () => {
             stats.map((item, i)=>{
               return (
                 <div key={i} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
+                  <span className="font-bold text-4xl">+</span>
                   <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold"/>
                   <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
                 </div>
