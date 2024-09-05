8216c861e00b599b42c306a35f57b12050c91aeb
Gil <gilvicentevieira@gmail.com>
2024-09-05 23:37:40 +0100

    resume page, visual upgrades

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -9,9 +9,8 @@ const about = {
   description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos repellat ipsum deleniti, culpa nihil maiores? Error, totam.",
   info: [
     { fieldName: "Name", fieldvalue: "Gil Vieira" },
-    { fieldName: "Phone", fieldvalue: "+40 321 654 678" },
-    { fieldName: "Experience", fieldvalue: "12+ Years" },
-    { fieldName: "Skype", fieldvalue: "gilvicentevieira" },
+    { fieldName: "Phone", fieldvalue: "+351 910 466 938" },
+    { fieldName: "Experience", fieldvalue: "+7 Years" },
     { fieldName: "Nationality", fieldvalue: "Portuguese" },
     { fieldName: "Email", fieldvalue: "gilvicentevieira@gmail.com" },
     { fieldName: "Languages", fieldvalue: "Portuguese, English, Spanish" }
@@ -24,12 +23,10 @@ const experience = {
   title: 'My experience',
   description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos repellat ipsum deleniti, culpa nihil maiores? Error, totam.',
   items: [
-    { company: "Tech Solutions Inc.", position: "Full Stack Developer", duration: "2022 - Present" },
-    { company: "Web Design Studio", position: "Front-End developer Intern", duration: "Summer 2021" },
-    { company: "E-commerce Startup", position: "Freelance Web Developer", duration: "2020 - 2021" },
-    { company: "Tech academy", position: "Teaching Assistant", duration: "2019 - 2020" },
-    { company: "Digital Agency", position: "UI/UX Designer", duration: "2018 - 2019" },
-    { company: "Software Development Firm", position: "Junior Developer", duration: "2017 - 2018" },
+    { company: "Nau21, Software For The Future", position: "Sofware Engineer Intern", duration: "2017" },
+    { company: "Nau21, Software For The Future", position: "Full Stack Web Engineer", duration: "2017-2023" },
+    { company: "SageByte", position: "Frontend Engineer (Freelance)", duration: "2019-2021" },
+    { company: "SageByte", position: "Frontend Engineer (Freelance)", duration: "2024" },
   ]
 }
 
@@ -39,11 +36,20 @@ const education = {
   title: 'My education',
   description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos repellat ipsum deleniti, culpa nihil maiores? Error, totam.',
   items: [
-    { institution: "Online Course Platform", position: "Full Stack Web Development Bootcamp", duration: "2023" },
-    { institution: "Codeacademy", position: "Front-end Track", duration: "2022" },
-    { institution: "Online Course Platform", position: "Programming Course", duration: "2020-2021" },
-    { institution: "Tech Institute", position: "Certified Web Developer", duration: "2019" },
-    { institution: "Designer School", position: "Diploma in Graphic Design", duration: "2016-2018" }
+    { institution: "The Massachusetts Institute of Technology through edX", position: "Introduction to Computer Science and Programming", duration: "2014" },
+    { institution: "The Massachusetts Institute of Technology through edX", position: "Introduction to Computational Thinking", duration: "2014" },
+    { institution: "Instituto Superior de Engenharia do Porto", position: "Software Engineering Degree", duration: "2014-2017" },
+    { institution: "Udemy", position: "JavaScript - The Complete Guide", duration: "2020" },
+    { institution: "Udemy", position: "CSS - The Complete Guide", duration: "2020" },
+    { institution: "Udemy", position: "React - The Complete Guide", duration: "2020" },
+    { institution: "Udemy", position: "NextJS and OpenAI", duration: "2023" },    
+    { institution: "Udemy", position: "ChatGPT Complete Guide: ChatGPT, Midjourney & More", duration: "2024" },
+    { institution: "Udemy", position: "React Native - The Practical Guide", duration: "2024" },
+    { institution: "Udemy", position: "Vue - The Complete Guide", duration: "2024" },
+    { institution: "Udemy", position: "Angular - The Complete Guide", duration: "2024" },
+    { institution: "Udemy", position: "UnderStanding TypeScript - The Complete Guide", duration: "2024" },
+    { institution: "Udemy", position: "NodeJS - The Complete Guide", duration: "2024" },
+    { institution: "Udemy", position: "MicroFrontends with React", duration: "2024" },
   ]
 }
 
@@ -78,7 +84,7 @@ const Resume = () => {
       }}
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
-      <div className="container mx-auto">
+      <div className="container">
         <Tabs defaultValue="Experience" className="flex flex-col xl:flex-row gap-[60px]">
           <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
             <TabsTrigger value="experience">Experience</TabsTrigger>
@@ -94,16 +100,16 @@ const Resume = () => {
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{experience.title}</h3>
                 <p className="max-w-[600px] text-white/80 mx-auto xl:mx-0">{experience.description}</p>
-                <ScrollArea className="h-[400px]">
+                <ScrollArea className="h-full">
                   <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                     {experience.items.map((item, i) => {
                       return (
-                        <li key={i} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                        <li key={i} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-start items-center lg:items-start gap-3">
                           <span className="text-accent">{item.duration}</span>
-                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <h3 className="text-sm max-w-[260px] text-center lg:text-left">{item.position}</h3>
                           <div className="flex items-center gap-3">
                             <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
-                            <p className="text-white/60">{item.company}</p>
+                            <p className="text-sm text-white/60">{item.company}</p>
                           </div>
                         </li>
                       )
@@ -117,16 +123,16 @@ const Resume = () => {
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{education.title}</h3>
                 <p className="max-w-[600px] text-white/80 mx-auto xl:mx-0">{education.description}</p>
-                <ScrollArea className="h-[400px]">
+                <ScrollArea className="h-full">
                   <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                     {education.items.map((item, i) => {
                       return (
-                        <li key={i} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                        <li key={i} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-start items-center lg:items-start gap-3">
                           <span className="text-accent">{item.duration}</span>
-                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <h3 className="text-sm text-center lg:text-left">{item.position}</h3>
                           <div className="flex items-center gap-3">
                             <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
-                            <p className="text-white/60">{item.institution}</p>
+                            <p className="text-sm text-white/60">{item.institution}</p>
                           </div>
                         </li>
                       )
@@ -165,17 +171,16 @@ const Resume = () => {
               </div>
 
             </TabsContent>
-            {/* education */}
+            {/* about */}
             <TabsContent value="about" className="w-full text-center xl:text-left">
               <div className="flex flex-col gap-[30px]">
                 <h3 className="text-4xl font-bold">{about.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
-                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                <ul className="w-full grid grid-cols-1 xl:grid-cols-2 gap-y-6 mx-auto xl:mx-0">
                   {about.info.map((item, i)=>{
                     return (
-                      <li key={i} className="flex items-center justify-center xl:justify-start gap-4">
+                      <li key={i} className="flex items-start justify-center xl:justify-start gap-4">
                         <span className="text-white/60">{item.fieldName} </span>
-                        <span className="text-xl">{item.fieldvalue}</span>
+                        <span className="break-words">{item.fieldvalue}</span>
                       </li>
                     )
                   })}
