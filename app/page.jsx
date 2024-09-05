cb8d09a2725cf1f68bc1b25eb5132f62b9b9f4ec
Gil <gilvicentevieira@gmail.com>
2024-09-05 22:08:25 +0100

    updated home page texts

--- app/page.jsx
+++ app/page.jsx
@@ -13,11 +13,11 @@ export default function Home() {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
+            <span className="text-xl">FullStack Web Engineer && Frontend Engineer</span>
             <h1 className="h1">Hello I'm <br/>
               <span className="text-accent">Gil Vieira</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.</p>
+            <p className="max-w-[500px] mb-9 text-white/80">Senior Engineer. <br/> JavaScript Ecosystem with many of it's libraries and tools. A bit of Java. <br/> In leading positions a few times and supervisor and tutor twice. </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
