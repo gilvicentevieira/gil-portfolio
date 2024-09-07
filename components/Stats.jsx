1a739e8899cb63e534c5f29b7136ed16e56c744a
Gil <gilvicentevieira@gmail.com>
2024-09-07 16:55:09 +0100

    improved Stats ui

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -13,14 +13,16 @@ const Stats = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+        <div className="flex flex-col xl:flex-row justify-center items-center gap-6">
           {
-            stats.map((item, i)=>{
+            stats.map((item, i) => {
               return (
                 <div key={i} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
-                  <span className="font-bold text-4xl">+</span>
-                  <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold"/>
-                  <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
+                  <div className="flex items-center">
+                    <span className="font-bold text-4xl">+</span>
+                    <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
+                  </div>
+                  <p className={`text-base leading-snug text-white/80`}>{item.text}</p>
                 </div>
               )
             })
